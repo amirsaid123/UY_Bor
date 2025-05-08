e6480ce1725f69c7b48ba7979f2cbfbc089e90c7
amirsaid123 <saidalosamigjonov071@gmail.com>
2025-05-08 16:47:30 +0500

    Changes to models

--- apps/models.py
+++ apps/models.py
@@ -31,6 +31,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     organization = models.CharField(max_length=255, blank=True)
     email = models.EmailField(unique=True, blank=True, null=True)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    role = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=False)
